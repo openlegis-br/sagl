80ae30e3d1f85d3665b0247691e752e013f7f09a
contato@openlegis.com.br <contato@openlegis.com.br@a4085b93-5678-905a-9f9e-c94047daf13e>
2014-05-24 21:45:09 +0000

    2.7: métodos ZSQL para assessoria parlamentar

--- instalacao/assessor.sql
+++ instalacao/assessor.sql
@@ -1,7 +1,7 @@
-CREATE TABLE `assessor_parlamentar` (
+CREATE TABLE IF NOT EXISTS `assessor_parlamentar` (
   `cod_assessor` int(11) NOT NULL AUTO_INCREMENT,
   `cod_parlamentar` tinyint(4) NOT NULL,
-  `nom_completo` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `nom_assessor` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `dat_nascimento` date DEFAULT NULL,
   `num_cpf` varchar(14) COLLATE utf8_unicode_ci DEFAULT NULL,
   `num_rg` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -13,11 +13,10 @@ CREATE TABLE `assessor_parlamentar` (
   `end_email` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `dat_nomeacao` date NOT NULL,
   `dat_exoneracao` date DEFAULT NULL,
+  `txt_observacao` text COLLATE utf8_unicode_ci,
   `col_username` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `ind_excluido` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`cod_assessor`),
-  UNIQUE KEY `idx_assessor_parlamentar` (`cod_assessor`,`cod_parlamentar`,`ind_excluido`),
-  KEY `idx_cod_parlamentar` (`cod_parlamentar`)
+  UNIQUE KEY `assessor_parlamentar` (`cod_assessor`,`cod_parlamentar`,`ind_excluido`),
+  KEY `cod_parlamentar` (`cod_parlamentar`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
-
-
