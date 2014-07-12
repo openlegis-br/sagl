0495c476e9a1cff2b2abf8e5b5598f6985355488
contato@openlegis.com.br <contato@openlegis.com.br@a4085b93-5678-905a-9f9e-c94047daf13e>
2014-07-12 16:44:49 +0000

    2.7: Rotina para tipo de emenda e reorganização de índice das tabelas auxiliares

--- branches/2.7/instalacao/assessor.sql
+++ branches/2.7/instalacao/assessor.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS `assessor_parlamentar` (
   `cod_assessor` int(11) NOT NULL AUTO_INCREMENT,
   `cod_parlamentar` int(11) NOT NULL,
   `nom_assessor` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `des_cargo` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `dat_nascimento` date DEFAULT NULL,
   `num_cpf` varchar(14) COLLATE utf8_unicode_ci DEFAULT NULL,
   `num_rg` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -20,3 +21,19 @@ CREATE TABLE IF NOT EXISTS `assessor_parlamentar` (
   UNIQUE KEY `assessor_parlamentar` (`cod_assessor`,`cod_parlamentar`,`ind_excluido`),
   KEY `cod_parlamentar` (`cod_parlamentar`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `tipo_emenda` (
+  `tip_emenda` int(11) NOT NULL AUTO_INCREMENT,
+  `des_tipo_emenda` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `ind_excluido` tinyint(4) NOT NULL,
+  PRIMARY KEY (`tip_emenda`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci PACK_KEYS=0 AUTO_INCREMENT=1 ;
+
+INSERT INTO `tipo_emenda` (`tip_emenda`, `des_tipo_emenda`, `ind_excluido`) VALUES
+(1, 'Aditiva', 0),
+(2, 'Modificativa', 0),
+(3, 'Substitutiva', 0),
+(4, 'Supressiva', 0),
+(5, 'Mensagem Aditiva', 0);
+
+
