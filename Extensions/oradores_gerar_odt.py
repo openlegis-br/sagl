16f7620120219bba34f04a2919c2ab624be9b43e
contato@openlegis.com.br <contato@openlegis.com.br@a4085b93-5678-905a-9f9e-c94047daf13e>
2014-11-01 19:28:23 +0000

    2.7: armazenamento de arquivos de pauta e oradores em formato ODT

--- Extensions/oradores_gerar_odt.py
+++ Extensions/oradores_gerar_odt.py
@@ -1,17 +1,16 @@
 ## encoding: utf-8 
 import os, urllib, cStringIO
-from appy.pod.renderer import Renderer    
- 
-def createReport(self, inf_basicas_dic, lst_oradores, lst_presidente):
+from appy.pod.renderer import Renderer
+
+def createReport(self, inf_basicas_dic, lst_oradores, lst_presidente, nom_arquivo):
     # Criacao ODT
     url = self.sapl_documentos.modelo.sessao_plenaria.absolute_url() + "/oradores.odt"
     template_file = cStringIO.StringIO(urllib.urlopen(url).read())
-    output_file_odt = "oradores.odt"
+    output_file_odt = "%s"%nom_arquivo
     renderer = Renderer(template_file, locals(), output_file_odt, pythonWithUnoPath='/usr/bin/python3',forceOoCall=True)
-    renderer.run()                                                                            
-    data = open(output_file_odt, "rb").read()                 
+    renderer.run()
+    data = open(output_file_odt, "rb").read()
     for file in [output_file_odt]:
-        os.unlink(file)                                                                                                      
-    self.REQUEST.RESPONSE.headers['Content-Type'] = 'application/vnd.oasis.opendocument.text'
-    self.REQUEST.RESPONSE.headers['Content-Disposition'] = 'attachment; filename="%s"'%output_file_odt
-    return data 
+        os.unlink(file)
+        self.sapl_documentos.oradores_expediente.manage_addFile(id=output_file_odt,file=data)
+
---
+++ Extensions/oradores_gerar_pdf.py
@@ -0,0 +1,19 @@
+## encoding: utf-8 
+import os, urllib, cStringIO
+from appy.pod.renderer import Renderer
+
+def convertFile(self,cod_sessao_plen):
+    # Conversao para PDF
+    nom_arquivo_odt = "%s"%cod_sessao_plen+'_oradores_expediente.odt'
+    nom_arquivo_pdf = "%s"%cod_sessao_plen+'_oradores_expediente.pdf'
+    url = self.sapl_documentos.oradores_expediente.absolute_url() + "/%s"%nom_arquivo_odt
+    odtFile = cStringIO.StringIO(urllib.urlopen(url).read())
+    output_file_pdf = os.path.normpath(nom_arquivo_pdf)
+    renderer = Renderer(odtFile,locals(),output_file_pdf,pythonWithUnoPath='/usr/bin/python3',forceOoCall=True)
+    renderer.run()
+    data = open(output_file_pdf, "rb").read()
+    for file in [output_file_pdf]:
+        self.sapl_documentos.oradores_expediente.manage_addProduct['ExtFile'].manage_addExtFile(id=file,title=file,file=file)
+        os.unlink(file)
+
+
