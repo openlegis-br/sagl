30e6adfc867e7328b451760d9753c0f2095ed3a5
openlegis-br <contato@openlegis.com.br>
2015-06-27 17:44:03 -0300

    melhoria paginação

--- branches/2.7_buildout/il/sapl/skins/pysc/PageListOutput_pysc.py
+++ branches/2.7_buildout/il/sapl/skins/pysc/PageListOutput_pysc.py
@@ -1,5 +1,4 @@
 ##parameters=linkLocation, page, step, length, **extras
-
 #########################################################################
 # Script (Python) "PageListOutput_pysc"
 # Randolpho St. John
@@ -18,9 +17,9 @@
 # similar to what you might find at the bottom of a Google search. It is inspired by the
 # functionality of a page list I once saw on a vBullitin web forum. This script is designed
 # to specifically emulate that page list system. However, the lessons learned from this script
-# can be adapted to other versions, such as the Google style mentioned above. 
+# can be adapted to other versions, such as the Google style mentioned above.
 #
-# This script returns html, a series of links to various pages of batch results. If the following 
+# This script returns html, a series of links to various pages of batch results. If the following
 # parameters (linkLocation="viewResults", page=3, step=20, length=300, sort="price") were sent
 # to the script, a link output might look like this:
 #
@@ -58,13 +57,13 @@ DisplayAll = 0                              # set flag to 1 to display every pag
                                             # will be ignored. WARNING: this could cause
                                             # quite a bit of trouble if you have an excessively
                                             # large list with a comparatively low batch size!
-PageRange = 5                               # number of pages before and after the current page
+PageRange = 3                               # number of pages before and after the current page
                                             # to display
 #
 # show item flags. Set to 1 to enable, set to 0 to disable.
 #
-showTotalPages = 1                          # show a total number of pages 
-showEllipses = 1                            # show a string signifying that not all pages numbers
+showTotalPages = 0                          # show a total number of pages
+showEllipses = 0                            # show a string signifying that not all pages numbers
                                             # are displayed
 showNext = 1                                # show a "Next Page" link
 showPrevious = 1                            # show a "Previous Page" link
@@ -73,20 +72,19 @@ showLast = 1                                # show a link to the last page
 #
 # output strings. Modify to alter what is returned by the script.
 #
-TotalPagesFormatString = "Páginas (%s):"      # String to use when showing total number of pages.
+TotalPagesFormatString = "Páginas (%s):"    # String to use when showing total number of pages.
                                             # Use %s to signify where the page number will be displayed.
 EllipsesString = "..."                      # String signifying that not all pages numbers are displayed
-NextString = ">>"                     # content of the "Next Page" link                  
-PreviousString = "<<"                 # content of the "Previous Page" link
-FirstString = "Primeira"                       # content of the "First Page" link
-LastString = "Última"                         # content of the "Last Page" link
-CurrentPageFormatString = "[%s]"            # String to use when showing the current page (which is not a link).
-                                            # Use %s to signify where the current page number is displayed.
+NextString = ">>"                           # content of the "Next Page" link                 
+PreviousString = "<<"                       # content of the "Previous Page" link
+FirstString = "<"                           # content of the "First Page" link
+LastString = ">"                            # content of the "Last Page" link
+CurrentPageFormatString = "<li class='pagination active'><a>%s</a></li>" # String to use when showing the current page (which is not a link).
+# Use %s to signify where the current page number is displayed.
 #
 #####################
 # End Modifications #
 #####################
-
 # validate parameters received
 try:
     length = int(length)
@@ -95,21 +93,15 @@ try:
     linkLocation = str(linkLocation)
     for item, value in extras.items():
         extras[item] = str(value)
-        
+       
 except:
     # this may be changed to suit your needs
     return "error building page list"
-
-
 # build the format string we'll be using for all page links. The only thing that will change
 # is the page number and the content of the link.
-linkString = '<a href="' + linkLocation + "?page=%s&amp;step=" + str(step)
-
+linkString = '<li class="pagination"><a href="' + linkLocation + "?page=%s&amp;step=" + str(step)
 for item, value in extras.items():
   if item == 'txt_assunto':
-    value = '%22'.join(value.split('\"'))
-    value = '%2B'.join(value.split('+'))
-  if item == 'txt_palavra_chave':
     value = '%22'.join(value.split('\"'))
     value = '%2B'.join(value.split('+'))
   if item == 'txt_texto':
@@ -117,28 +109,22 @@ for item, value in extras.items():
     value = '%2B'.join(value.split('+'))
  
   linkString = linkString + ("&%s=%s" % (item, value))
-linkString = linkString + '">%s</a> '
-
-
+linkString = linkString + '">%s</a></li>'
 # Find the total number of pages we need:
 numPages, remainder = divmod(length, step)
 if remainder != 0:
     numPages = numPages + 1
-
 # begin setting up the pages we'll display. This is currently hard-wired. Perhaps
 # some way of making the total number dynamic later?
 first = page - 2
 second = page - 1
 fourth = page + 1
 fifth = page + 2
-
 # build our returned output
 returnString = ""
 linkStringTemp = ""
-
 if showTotalPages:
     returnString = returnString + (TotalPagesFormatString %numPages)
-
 if showFirst:
     if page >= 2:
        linkStringTemp = 'page=1'.join(linkString.split('page=%s'))
@@ -146,50 +132,45 @@ if showFirst:
        returnString = returnString + linkStringTemp
     else:
        returnString = returnString + " " + FirstString + " "
-
 if showEllipses:
     returnString = returnString + " " + EllipsesString + " "
-
 if showPrevious:
     if page > 1:
         linkStringTemp = ('page=' + str(second)).join(linkString.split('page=%s'))
         linkStringTemp = PreviousString.join(linkStringTemp.split('%s'))
         returnString = returnString + linkStringTemp
     else:
-        returnString = returnString + " " + PreviousString + " "
-
+        returnString = PreviousString.join(linkStringTemp.split('%s'))
 if DisplayAll:
     LinkRange = range(1, numPages + 1)
-else:    
+else:   
     LinkRange = range(page - PageRange - max(0, (page + PageRange - numPages)), page + PageRange + 1 - min (0, (page - PageRange - 1)))
-
 for index in LinkRange:
     if index > 0 and index < page:
-	linkStringTemp = str(index).join(linkString.split('%s'))
+        linkStringTemp = str(index).join(linkString.split('%s'))
         returnString = returnString + linkStringTemp
     if index == page:
-        returnString = returnString + " " + (CurrentPageFormatString % page) + " "
+        returnString = returnString + (CurrentPageFormatString % page) 
     if index > page and index <= numPages:
-	linkStringTemp = str(index).join(linkString.split('%s'))
-	returnString = returnString + linkStringTemp
-    
+        linkStringTemp = str(index).join(linkString.split('%s'))
+        returnString = returnString + linkStringTemp
+   
 if showNext:
     if page < numPages:
-	linkStringTemp = ('page=' + str(fourth)).join(linkString.split('page=%s'))
-	linkStringTemp = NextString.join(linkStringTemp.split('%s'))
+        linkStringTemp = ('page=' + str(fourth)).join(linkString.split('page=%s'))
+        linkStringTemp = NextString.join(linkStringTemp.split('%s'))
         returnString = returnString + linkStringTemp
     else:
-        returnString = returnString + " " + NextString + " "
-        
+        returnString = returnString
+       
 if showEllipses:
     returnString = returnString + " " + EllipsesString + " "
-    
+   
 if showLast:
     if page <= numPages - 1:
-	linkStringTemp = ('page=' + str(numPages)).join(linkString.split('page=%s'))
-	linkStringTemp = LastString.join(linkStringTemp.split('%s'))
+        linkStringTemp = ('page=' + str(numPages)).join(linkString.split('page=%s'))
+        linkStringTemp = LastString.join(linkStringTemp.split('%s'))
         returnString = returnString + linkStringTemp
     else:
-        returnString = returnString + " " + LastString + " "
-
+        returnString = returnString
 return returnString
