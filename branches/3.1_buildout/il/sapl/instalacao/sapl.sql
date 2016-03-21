4c69f3a9317445217651ce2aeb2beb7621135718
OpenLegis Consultoria <contato@openlegis.com.br>
2016-03-21 14:25:15 -0300

    Cadastro completo de usuários, com dados pessoais, funcionais e vinculação com unidades de tramitação

--- branches/3.1_buildout/il/sapl/instalacao/sapl.sql
+++ branches/3.1_buildout/il/sapl/instalacao/sapl.sql
@@ -2634,6 +2634,63 @@ CREATE TABLE `unidade_tramitacao` (
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `cod_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `col_username` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `nom_completo` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `dat_nascimento` date DEFAULT NULL,
+  `num_cpf` varchar(14) COLLATE utf8_unicode_ci NOT NULL,
+  `num_rg` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `num_tit_eleitor` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `end_residencial` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `cod_localidade_resid` int(11) DEFAULT NULL,
+  `num_cep_resid` varchar(9) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `num_tel_resid` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `num_tel_celular` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `end_email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `nom_cargo` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `num_tel_comercial` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `num_ramal` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `txt_observacao` text COLLATE utf8_unicode_ci,
+  `ind_excluido` tinyint(4) NOT NULL,
+  PRIMARY KEY (`cod_usuario`),
+  KEY `idx_col_username` (`col_username`),
+  KEY `idx_cod_localidade` (`cod_localidade_resid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci PACK_KEYS=0 AUTO_INCREMENT=1 ;
+
+--
+--  RELACIONAMENTOS PARA A TABELA `usuario`:
+--   `cod_localidade_resid`
+--       `localidade` -> `cod_localidade`
+--
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario_unid_tram`
+--
+
+CREATE TABLE `usuario_unid_tram` (
+  `cod_usuario` int(11) NOT NULL,
+  `cod_unid_tramitacao` int(11) NOT NULL,
+  `ind_excluido` tinyint(4) NOT NULL DEFAULT '0',
+  UNIQUE KEY `PRIMARY_KEY` (`cod_usuario`,`cod_unid_tramitacao`),
+  KEY `idx_usuario` (`cod_usuario`),
+  KEY `idx_unid_tramitacao` (`cod_unid_tramitacao`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+--  RELACIONAMENTOS PARA A TABELA `usuario_unid_tram`:
+--   `cod_unid_tramitacao`
+--       `unidade_tramitacao` -> `cod_unid_tramitacao`
+--   `cod_usuario`
+--       `usuario` -> `cod_usuario`
+--
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `vinculo_norma_juridica`
 --
 
