5d3997edb8e95eb23a1f7501cd7ac2a597d52fce
OpenLegis Consultoria <contato@openlegis.com.br>
2016-08-04 14:25:11 -0300

    Rotina para ajuste automático de permissão para visualização de documentos administrativos

---
+++ branches/3.1_buildout/il/sapl/skins/cadastros/auxiliares/tipo_documento_administrativo/ajusta_permissao.py
@@ -0,0 +1,17 @@
+## Script (Python) "ajusta_permissao.py"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=tip_documento, ind_publico
+##title=
+REQUEST=context.REQUEST
+for documento in context.zsql.documento_administrativo_obter_publico_zsql(tip_documento=tip_documento,ind_publico=ind_publico,ind_excluido=0):
+ filename = str(documento.cod_documento) + "_texto_integral.pdf"
+ if hasattr(context.sapl_documentos.administrativo, filename):
+   documento = getattr(context.sapl_documentos.administrativo,filename)
+   if REQUEST.ind_publico == 0:
+     documento.manage_permission('View', roles=['Manager','Operador','Operador Modulo Administrativo'], acquire=1)
+   if REQUEST.ind_publico == 1:
+     documento.manage_permission('View', roles=['Anonymous','Manager','Operador','Operador Modulo Administrativo'], acquire=0)
