43856c7ed621489e80824eb1c6d880a9172fbda4
OpenLegis Consultoria <contato@openlegis.com.br>
2016-08-07 12:29:08 -0300

    Corrige script permissão - documento administrativo

--- branches/3.1_buildout/il/sapl/skins/cadastros/auxiliares/tipo_documento_administrativo/ajusta_permissao.py
+++ branches/3.1_buildout/il/sapl/skins/cadastros/auxiliares/tipo_documento_administrativo/ajusta_permissao.py
@@ -7,11 +7,12 @@
 ##parameters=tip_documento, ind_publico
 ##title=
 REQUEST=context.REQUEST
-for documento in context.zsql.documento_administrativo_obter_publico_zsql(tip_documento=tip_documento,ind_publico=ind_publico,ind_excluido=0):
+for documento in context.zsql.documento_administrativo_pesquisar_publico_zsql(tip_documento=tip_documento,ind_publico=ind_publico,ind_excluido=0):
  filename = str(documento.cod_documento) + "_texto_integral.pdf"
  if hasattr(context.sapl_documentos.administrativo, filename):
-   documento = getattr(context.sapl_documentos.administrativo,filename)
-   if REQUEST.ind_publico == 0:
+   if context.REQUEST.ind_publico == 0:
+     documento = getattr(context.sapl_documentos.administrativo,filename)
      documento.manage_permission('View', roles=['Manager','Operador','Operador Modulo Administrativo'], acquire=1)
-   if REQUEST.ind_publico == 1:
+   if context.REQUEST.ind_publico == 1:
+     documento = getattr(context.sapl_documentos.administrativo,filename)
      documento.manage_permission('View', roles=['Anonymous','Manager','Operador','Operador Modulo Administrativo'], acquire=0)
