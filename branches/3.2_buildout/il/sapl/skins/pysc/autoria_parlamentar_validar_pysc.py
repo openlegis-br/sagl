167d9ac56162131fb63498a0b645dae17968a7ff
OpenLegis Consultoria <contato@openlegis.com.br>
2016-11-10 20:24:29 -0200

    Valida autoria e presença de parlamentares em períodos de afastamento

--- branches/3.2_buildout/il/sapl/skins/pysc/autoria_parlamentar_validar_pysc.py
+++ branches/3.2_buildout/il/sapl/skins/pysc/autoria_parlamentar_validar_pysc.py
@@ -13,15 +13,22 @@
   Argumentos: txt_dat_apresentacao e cod_parlamentar  --> retorna 1-verdadeiro ou 0-falso.
 
 """  
+dat_apresentacao = DateTime (context.pysc.data_converter_pysc(txt_dat_apresentacao))
 codigo = str(cod_parlamentar)
 autores = context.zsql.autores_obter_zsql(txt_dat_apresentacao=txt_dat_apresentacao) or []
 
 for p in autores:
   s = str(p.cod_parlamentar)
   if s == codigo:
-    return 1
+    for item in context.zsql.afastamento_obter_zsql(cod_parlamentar=codigo):
+     dat_inicio = DateTime(item.dat_inicio)
+     dat_fim = DateTime(item.dat_fim)
+     if dat_inicio < dat_apresentacao < dat_fim:
+       return 0
+    else: 
+     return 1
 
-return 0
+
 
 
 
