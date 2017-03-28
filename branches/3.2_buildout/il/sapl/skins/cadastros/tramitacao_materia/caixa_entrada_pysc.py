4c7a741ff5015666b392c72368c6fc5300c9303a
OpenLegis Consultoria <contato@openlegis.com.br>
2017-03-28 19:34:16 -0300

    Regras de exibição na caixa de entrada de tramitações eletrônicas

--- branches/3.2_buildout/il/sapl/skins/cadastros/tramitacao_materia/caixa_entrada_pysc.py
+++ branches/3.2_buildout/il/sapl/skins/cadastros/tramitacao_materia/caixa_entrada_pysc.py
@@ -8,13 +8,19 @@
 ##title=
 ##
 unidades = []
+dic = {}
 for unidade in context.zsql.usuario_unid_tram_obter_zsql(cod_usuario=cod_usuario):
-  cod_unid_tramitacao = int(unidade.cod_unid_tramitacao)
-  unidades.append(cod_unid_tramitacao)
+  dic["cod_unid_tramitacao"]  = int(unidade.cod_unid_tramitacao)
+  dic["ind_responsavel"] = int(unidade.ind_responsavel)
+  unidades.append(dic)
 
 tramitacoes = []
 for unidade in unidades:
-  for tramitacao in context.zsql.tramitacao_obter_zsql(cod_unid_tram_destino=unidade,ind_ult_tramitacao=1,ind_encaminha=1,ind_recebido=0,ind_retorno_tramitacao=1,ind_tramitacao=1):
-    tramitacoes.append(int(tramitacao.cod_tramitacao))
+  if dic["ind_responsavel"] == 1:
+    for tramitacao in context.zsql.tramitacao_obter_zsql(cod_unid_tram_destino= dic["cod_unid_tramitacao"],ind_ult_tramitacao=1,ind_encaminha=1,ind_recebido=0,ind_retorno_tramitacao=1,ind_tramitacao=1):
+      tramitacoes.append(int(tramitacao.cod_tramitacao))
+  else:
+    for tramitacao in context.zsql.tramitacao_obter_zsql(cod_unid_tram_destino= dic["cod_unid_tramitacao"],cod_usuario_dest=cod_usuario,ind_ult_tramitacao=1,ind_encaminha=1,ind_recebido=0,ind_retorno_tramitacao=1,ind_tramitacao=1):
+      tramitacoes.append(int(tramitacao.cod_tramitacao))
 
 return tramitacoes
