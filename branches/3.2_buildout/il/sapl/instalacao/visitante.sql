b030ef6ebca0c60e260a52cd190a382ef243d04b
OpenLegis Consultoria <contato@openlegis.com.br>
2017-08-13 21:32:12 -0300

    Módulo de controle de visitantes e atendimento ao público nos gabinetes

---
+++ branches/3.2_buildout/il/sapl/instalacao/visitante.sql
@@ -0,0 +1,110 @@
+CREATE TABLE `logradouro` (
+  `cod_logradouro` int(11) NOT NULL,
+  `nom_logradouro` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `nom_bairro` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `num_cep` varchar(9) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `cod_localidade` int(11) DEFAULT NULL,
+  `ind_excluido` tinyint(4) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+ALTER TABLE `logradouro`
+  ADD PRIMARY KEY (`cod_logradouro`),
+  ADD KEY `num_cep` (`num_cep`),
+  ADD KEY `cod_localidade` (`cod_localidade`);
+ALTER TABLE `logradouro` ADD FULLTEXT KEY `nom_logradouro` (`nom_logradouro`);
+
+ALTER TABLE `logradouro`
+  MODIFY `cod_logradouro` int(11) NOT NULL AUTO_INCREMENT;
+
+
+
+CREATE TABLE `funcionario` (
+  `cod_funcionario` int(11) NOT NULL,
+  `nom_funcionario` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `cod_usuario` int(11) DEFAULT NULL,
+  `des_cargo` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `dat_cadastro` date NOT NULL,
+  `ind_ativo` tinyint(4) NOT NULL DEFAULT '1',
+  `ind_excluido` tinyint(4) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+ALTER TABLE `funcionario`
+  ADD PRIMARY KEY (`cod_funcionario`),
+  ADD INDEX(`cod_usuario`);
+
+ALTER TABLE `funcionario`
+  MODIFY `cod_funcionario` int(11) NOT NULL AUTO_INCREMENT;
+
+
+
+CREATE TABLE `pessoa` (
+  `cod_pessoa` int(11) NOT NULL,
+  `nom_pessoa` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `doc_identidade` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `dat_nascimento` date DEFAULT NULL,
+  `sex_pessoa` char(1) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `des_estado_civil` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `nom_conjuge` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `num_dependentes` tinytext COLLATE utf8_unicode_ci,
+  `num_tit_eleitor` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `cod_logradouro` int(11) DEFAULT NULL,
+  `end_residencial` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `num_imovel` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `nom_bairro` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `num_cep` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
+  `nom_cidade` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `des_tempo_residencia` varchar(25) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `num_telefone` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `num_celular` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `end_email` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `des_profissao` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `des_local_trabalho` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `txt_observacao` text COLLATE utf8_unicode_ci,
+  `dat_atualizacao` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `ind_excluido` tinyint(4) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+ALTER TABLE `pessoa`
+  ADD PRIMARY KEY (`cod_pessoa`),
+  ADD KEY `num_cep` (`num_cep`),
+  ADD KEY `cod_logradouro` (`cod_logradouro`),
+  ADD KEY `nom_cidade` (`nom_cidade`),
+  ADD KEY `dat_nascimento` (`dat_nascimento`),
+  ADD KEY `des_profissao` (`des_profissao`),
+  ADD KEY `des_estado_civil` (`des_estado_civil`),
+  ADD KEY `sex_visitante` (`sex_pessoa`),
+  ADD KEY `nom_bairro` (`nom_bairro`);
+ALTER TABLE `pessoa` ADD FULLTEXT KEY `nom_pessoa` (`nom_pessoa`);
+ALTER TABLE `pessoa` ADD FULLTEXT KEY `nom_conjuge` (`nom_conjuge`);
+ALTER TABLE `pessoa` ADD FULLTEXT KEY `idx_busca` (`doc_identidade`);
+ALTER TABLE `pessoa` ADD FULLTEXT KEY `end_residencial` (`end_residencial`);
+ALTER TABLE `pessoa` ADD FULLTEXT KEY `doc_identidade` (`doc_identidade`);
+
+ALTER TABLE `pessoa`
+  MODIFY `cod_pessoa` int(11) NOT NULL AUTO_INCREMENT;
+
+
+
+CREATE TABLE `visita` (
+  `cod_visita` int(11) NOT NULL,
+  `cod_pessoa` int(11) NOT NULL,
+  `dat_entrada` datetime NOT NULL,
+  `cod_funcionario` int(11) NOT NULL,
+  `num_cracha` int(11) DEFAULT NULL,
+  `dat_saida` datetime DEFAULT NULL,
+  `txt_atendimento` text COLLATE utf8_unicode_ci,
+  `des_situacao` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `dat_solucao` date DEFAULT NULL,
+  `txt_observacao` text COLLATE utf8_unicode_ci,
+  `ind_excluido` tinyint(4) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+ALTER TABLE `visita`
+  ADD PRIMARY KEY (`cod_visita`),
+  ADD KEY `cod_funcionario` (`cod_funcionario`),
+  ADD KEY `cod_pessoa` (`cod_pessoa`) USING BTREE,
+  ADD KEY `dat_entrada` (`dat_entrada`),
+  ADD KEY `des_situacao` (`des_situacao`);
+
+ALTER TABLE `visita`
+  MODIFY `cod_visita` int(11) NOT NULL AUTO_INCREMENT;
---
+++ branches/3.2_buildout/il/sapl/skins/cadastros/atendimento/pdf_gerar_etiquetas_pessoas_pysc.py
@@ -0,0 +1,44 @@
+## Script (Python) "pdf_gerar_etiquetas_pessoas_pysc"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters= cod_funcionario_corrente, txt_dat_visita, txt_dat_visita2, lst_mes_aniversario, rad_sex_pessoa, txt_des_estado_civil, rad_filhos, txt_des_profissao, txt_des_local_trabalho, txt_end_residencial, txt_nom_bairro, txt_num_cep, txt_nom_cidade
+##title=
+##
+
+REQUEST = context.REQUEST
+RESPONSE =  REQUEST.RESPONSE
+session = REQUEST.SESSION
+
+results =  context.zsql.pessoa_pesquisar_zsql(
+                                               cod_funcionario=REQUEST['cod_funcionario_corrente'],
+                                               dat_visita=REQUEST['txt_dat_visita'],
+                                               dat_visita2=REQUEST['txt_dat_visita2'],
+                                               mes_aniversario=REQUEST['lst_mes_aniversario'],
+                                               sex_pessoa=REQUEST['rad_sex_pessoa'],
+                                               des_estado_civil=REQUEST['txt_des_estado_civil'],
+                                               rad_filhos=REQUEST['rad_filhos'],
+                                               des_profissao=REQUEST['txt_des_profissao'],
+                                               des_local_trabalho=REQUEST['txt_des_local_trabalho'],
+                                               end_residencial=REQUEST['txt_end_residencial'],
+                                               nom_bairro=REQUEST['txt_nom_bairro'],
+                                               num_cep=REQUEST['txt_num_cep'],
+                                               nom_cidade=REQUEST['txt_nom_cidade']
+                                               )
+dados = []
+for row in results:
+    r=[]
+    # Label, Data
+    if row[1]!=None:
+     r.append(row[1].encode( "utf-8" ))
+    if row[10]!=None:
+     r.append(row[10].encode( "utf-8" )+', '+row[11].encode( "utf-8" ))
+    if row[12]!=None:
+     r.append(row[12].encode( "utf-8" )+' - CEP  '+row[13].encode( "utf-8" ))
+    if row[14]!=None:
+     r.append(row[14].encode( "utf-8" ))
+    dados.append(r)
+return context.pdflabels(dados)
+
