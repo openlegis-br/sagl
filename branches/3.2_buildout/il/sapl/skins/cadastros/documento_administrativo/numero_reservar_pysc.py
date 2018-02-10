c737c4e919b1106d3de3503e915203c65ae7cf91
OpenLegis Consultoria <contato@openlegis.com.br>
2018-02-10 16:52:07 -0200

    Função para reserva de numeração de docs administrativos

---
+++ branches/3.2_buildout/il/sapl/skins/cadastros/documento_administrativo/numero_reservar_pysc.py
@@ -0,0 +1,34 @@
+## Script (Python) "numero_reservar_pysc"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=tip_documento, txt_interessado, txt_assunto, txt_qtde
+##title=
+##
+
+ano_documento = DateTime().strftime("%Y")
+dat_documento = DateTime().strftime("%Y/%m/%d")
+for numero in context.zsql.numero_documento_administrativo_obter_zsql(tip_documento=tip_documento, ano_documento=ano_documento):
+   numero_inicial = numero.novo_numero 
+qtde = int(txt_qtde)
+ind_tramitacao = 1
+ind_excluido = 0
+
+for i in range(qtde):
+  num_documento = i + numero_inicial
+  ano_documento = ano_documento
+  tip_documento = tip_documento
+  txt_interessado = txt_interessado
+  context.zsql.documento_administrativo_incluir_zsql(num_documento = num_documento, 
+                                                     ano_documento = ano_documento, 
+                                                     dat_documento = DateTime().strftime("%Y/%m/%d"), 
+                                                     tip_documento = tip_documento, 
+                                                     txt_interessado = txt_interessado,
+                                                     txt_assunto = txt_assunto,
+                                                     ind_tramitacao = ind_tramitacao,
+                                                     ind_excluido = ind_excluido) 
+
+return 1
+
