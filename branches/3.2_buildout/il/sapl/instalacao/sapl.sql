58473dd9461d129157631879478926bc3239ad79
OpenLegis Consultoria <contato@openlegis.com.br>
2019-01-18 22:46:27 -0200

    Adiciona tabela documento_comissao

--- branches/3.2_buildout/il/sapl/instalacao/sapl.sql
+++ branches/3.2_buildout/il/sapl/instalacao/sapl.sql
@@ -478,7 +478,7 @@ CREATE TABLE `documento_comissao` (
   `cod_comissao` int(11) NOT NULL,
   `dat_documento` date NOT NULL,
   `txt_descricao` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `txt_observacao` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `txt_observacao` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL,
   `ind_excluido` tinyint(4) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
