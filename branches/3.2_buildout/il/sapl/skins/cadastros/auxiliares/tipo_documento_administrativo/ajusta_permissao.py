b6a389b93d3f67673d3be1ffb96a15cd0bd3345d
OpenLegis Consultoria <contato@openlegis.com.br>
2019-02-21 14:54:07 -0300

    DTML - obs periodo comp mesa

--- branches/3.2_buildout/il/sapl/skins/cadastros/auxiliares/tipo_documento_administrativo/ajusta_permissao.py
+++ branches/3.2_buildout/il/sapl/skins/cadastros/auxiliares/tipo_documento_administrativo/ajusta_permissao.py
@@ -17,7 +17,7 @@ for documento in context.zsql.documento_administrativo_pesquisar_publico_zsql(ti
      documento.manage_permission('View', roles=['Authenticated', 'Manager','Operador','Operador Modulo Administrativo'], acquire=1)
    if context.REQUEST.ind_publico == 1:
      documento = getattr(context.sapl_documentos.administrativo,filename)
-     documento.manage_permission('View', roles=['Anonymous','Manager','Operador','Operador Modulo Administrativo'], acquire=0)
+     documento.manage_permission('View', roles=['Anonymous','Manager','Operador','Operador Materia','Operador Modulo Administrativo'], acquire=0)
      documento.manage_permission('Delete objects', roles=['Manager','Operador','Operador Materia','Operador Modulo Administrativo'], acquire=0)
  if hasattr(context.sapl_documentos.administrativo, assinado):
    if context.REQUEST.ind_publico == 0:
@@ -25,7 +25,7 @@ for documento in context.zsql.documento_administrativo_pesquisar_publico_zsql(ti
      documento.manage_permission('View', roles=['Authenticated', 'Manager','Operador','Operador Modulo Administrativo'], acquire=1)
    if context.REQUEST.ind_publico == 1:
      documento = getattr(context.sapl_documentos.administrativo,assinado)
-     documento.manage_permission('View', roles=['Anonymous','Manager','Operador','Operador Modulo Administrativo'], acquire=0)
+     documento.manage_permission('View', roles=['Anonymous','Manager','Operador','Operador Materia','Operador Modulo Administrativo'], acquire=0)
      documento.manage_permission('Delete objects', roles=['Manager','Operador','Operador Materia','Operador Modulo Administrativo'], acquire=0)
 
 for documento in context.zsql.documento_acessorio_administrativo_obter_zsql(tip_documento=tip_documento,ind_excluido=0):
@@ -36,5 +36,5 @@ for documento in context.zsql.documento_acessorio_administrativo_obter_zsql(tip_
      documento.manage_permission('View', roles=['Authenticated','Manager','Operador','Operador Modulo Administrativo'], acquire=1)
    if context.REQUEST.ind_publico == 1:
      documento = getattr(context.sapl_documentos.administrativo,filename_acessorio)
-     documento.manage_permission('View', roles=['Anonymous','Manager','Operador','Operador Modulo Administrativo'], acquire=0)
+     documento.manage_permission('View', roles=['Anonymous','Manager','Operador','Operador Materia','Operador Modulo Administrativo'], acquire=0)
      documento.manage_permission('Delete objects', roles=['Manager','Operador','Operador Materia','Operador Modulo Administrativo'], acquire=0)
