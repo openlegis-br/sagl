4d728182b3f4ef969f4f24f2d7a247954d161db4
OpenLegis Consultoria <contato@openlegis.com.br>
2020-01-20 03:51:35 -0300

    Ajustes incorporação / protocolo

--- branches/3.2_buildout/il/sapl/skins/pysc/protocolo_salvar_como_texto_integral_documento1_pysc.py
+++ branches/3.2_buildout/il/sapl/skins/pysc/protocolo_salvar_como_texto_integral_documento1_pysc.py
@@ -7,20 +7,20 @@
 ##parameters=cod_protocolo, cod_documento, ind_sobrescrever=1
 ##title=
 ##
-ok = 1
-id = str(cod_documento) + '_texto_integral.pdf'
+ok = 0
+id = str(cod_documento) + '_texto_integral_signed.pdf'
 
 try:
     doc = context.sapl_documentos.administrativo[id]
     if (int(ind_sobrescrever)==1):
         doc=''     
         context.sapl_documentos.administrativo.manage_delObjects(id)
-        tmp_copy = context.sapl_documentos.protocolo.manage_copyObjects(ids=str(cod_protocolo)+'_protocolo.pdf')
+        tmp_copy = context.sapl_documentos.protocolo.manage_copyObjects(ids=str(cod_protocolo)+'_protocolo_signed.pdf')
         tmp_id = context.sapl_documentos.administrativo.manage_pasteObjects(tmp_copy)[0]['new_id']
         context.sapl_documentos.administrativo.manage_renameObjects(ids=list([tmp_id]),new_ids=list([id]))
         ok = 1
 except KeyError:
-    tmp_copy = context.sapl_documentos.protocolo.manage_copyObjects(ids=str(cod_protocolo)+ '_protocolo.pdf')
+    tmp_copy = context.sapl_documentos.protocolo.manage_copyObjects(ids=str(cod_protocolo)+ '_protocolo_signed.pdf')
     tmp_id = context.sapl_documentos.administrativo.manage_pasteObjects(tmp_copy)[0]['new_id']
     context.sapl_documentos.administrativo.manage_renameObjects(ids=list([tmp_id]),new_ids=list([id]))
     ok = 1
--- branches/3.2_buildout/il/sapl/skins/pysc/protocolo_salvar_como_texto_integral_materia1_pysc.py
+++ branches/3.2_buildout/il/sapl/skins/pysc/protocolo_salvar_como_texto_integral_materia1_pysc.py
@@ -8,18 +8,18 @@
 ##title=
 ##
 ok = 0
-id = str(cod_materia) + '_texto_integral.pdf'
+id = str(cod_materia) + '_texto_integral_signed.pdf'
 try:
     doc = context.sapl_documentos.materia[id]
     if (int(ind_sobrescrever)==1):
         doc=''     
         context.sapl_documentos.materia.manage_delObjects(id)
-        tmp_copy = context.sapl_documentos.protocolo.manage_copyObjects(ids=str(cod_protocolo)+'_protocolo.pdf')
+        tmp_copy = context.sapl_documentos.protocolo.manage_copyObjects(ids=str(cod_protocolo)+'_protocolo_signed.pdf')
         tmp_id = context.sapl_documentos.materia.manage_pasteObjects(tmp_copy)[0]['new_id']
         context.sapl_documentos.materia.manage_renameObjects(ids=list([tmp_id]),new_ids=list([id]))
         ok = 1
 except KeyError:
-    tmp_copy = context.sapl_documentos.protocolo.manage_copyObjects(ids=str(cod_protocolo)+ '_protocolo.pdf')
+    tmp_copy = context.sapl_documentos.protocolo.manage_copyObjects(ids=str(cod_protocolo)+ '_protocolo_signed.pdf')
     tmp_id = context.sapl_documentos.materia.manage_pasteObjects(tmp_copy)[0]['new_id']
     context.sapl_documentos.materia.manage_renameObjects(ids=list([tmp_id]),new_ids=list([id]))
     ok = 1
