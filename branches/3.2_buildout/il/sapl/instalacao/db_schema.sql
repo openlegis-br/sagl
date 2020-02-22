fe3643d9304faa6223c637dab38386f1d0de4959
OpenLegis Consultoria <contato@openlegis.com.br>
2020-02-22 13:14:34 -0300

    Pesquisa de matérias acessórias

--- branches/3.2_buildout/il/sapl/instalacao/db_schema.sql
+++ branches/3.2_buildout/il/sapl/instalacao/db_schema.sql
@@ -587,6 +587,7 @@ CREATE TABLE IF NOT EXISTS `emenda` (
   `tip_emenda` int(11) NOT NULL,
   `num_emenda` int(11) NOT NULL,
   `cod_materia` int(11) NOT NULL,
+  `cod_autor` int(11) DEFAULT NULL,
   `num_protocolo` int(11) DEFAULT NULL,
   `dat_apresentacao` date DEFAULT NULL,
   `txt_ementa` varchar(400) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -596,7 +597,8 @@ CREATE TABLE IF NOT EXISTS `emenda` (
   PRIMARY KEY (`cod_emenda`),
   KEY `idx_cod_materia` (`cod_materia`),
   KEY `idx_tip_emenda` (`tip_emenda`),
-  KEY `idx_emenda` (`cod_emenda`,`tip_emenda`,`cod_materia`) USING BTREE
+  KEY `idx_emenda` (`cod_emenda`,`tip_emenda`,`cod_materia`) USING BTREE,
+  KEY `cod_autor` (`cod_autor`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `encerramento_presenca` (
@@ -836,6 +838,7 @@ CREATE TABLE IF NOT EXISTS `liderancas_partidarias` (
   KEY `cod_sessao_plen` (`cod_sessao_plen`),
   KEY `cod_partido` (`cod_partido`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci PACK_KEYS=0;
+
 CREATE TABLE IF NOT EXISTS `listaAutores` (
 `cod_autor` varchar(11)
 ,`nom_autor_join` varchar(100)
@@ -916,6 +919,9 @@ CREATE TABLE IF NOT EXISTS `materia_apresentada_sessao` (
   `cod_ordem` int(11) NOT NULL AUTO_INCREMENT,
   `cod_sessao_plen` int(11) NOT NULL,
   `cod_materia` int(11) DEFAULT NULL,
+  `cod_emenda` int(11) DEFAULT NULL,
+  `cod_substitutivo` int(11) DEFAULT NULL,
+  `cod_doc_acessorio` int(11) DEFAULT NULL,
   `cod_documento` int(11) DEFAULT NULL,
   `dat_ordem` date NOT NULL,
   `txt_observacao` text COLLATE utf8_unicode_ci,
@@ -926,8 +932,9 @@ CREATE TABLE IF NOT EXISTS `materia_apresentada_sessao` (
   KEY `idx_apres_datord` (`dat_ordem`),
   KEY `cod_sessao_plen` (`cod_sessao_plen`),
   KEY `idx_cod_documento` (`cod_documento`),
-  KEY `cod_materia` (`cod_materia`),
-  KEY `cod_materia_2` (`cod_materia`)
+  KEY `cod_emenda` (`cod_emenda`),
+  KEY `cod_substitutivo` (`cod_substitutivo`),
+  KEY `cod_doc_acessorio` (`cod_doc_acessorio`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci PACK_KEYS=0;
 
 CREATE TABLE IF NOT EXISTS `materia_legislativa` (
@@ -972,6 +979,7 @@ CREATE TABLE IF NOT EXISTS `materia_legislativa` (
   KEY `idx_matleg_ident` (`ind_excluido`,`tip_id_basica`,`ano_ident_basica`,`num_ident_basica`) USING BTREE,
   KEY `idx_tramitacao` (`ind_tramitacao`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci PACK_KEYS=0;
+
 CREATE TABLE IF NOT EXISTS `mesaAtual` (
 `cod_parlamentar` int(11)
 ,`des_cargo` varchar(50)
@@ -1530,6 +1538,7 @@ CREATE TABLE IF NOT EXISTS `substitutivo` (
   `cod_substitutivo` int(11) NOT NULL AUTO_INCREMENT,
   `num_substitutivo` int(11) NOT NULL,
   `cod_materia` int(11) NOT NULL,
+  `cod_autor` int(11) DEFAULT NULL,
   `num_protocolo` int(11) DEFAULT NULL,
   `dat_apresentacao` date DEFAULT NULL,
   `txt_ementa` text COLLATE utf8_unicode_ci,
@@ -1537,7 +1546,8 @@ CREATE TABLE IF NOT EXISTS `substitutivo` (
   `ind_excluido` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`cod_substitutivo`),
   KEY `idx_cod_materia` (`cod_materia`),
-  KEY `idx_substitutivo` (`cod_substitutivo`,`cod_materia`) USING BTREE
+  KEY `idx_substitutivo` (`cod_substitutivo`,`cod_materia`) USING BTREE,
+  KEY `cod_autor` (`cod_autor`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `tipo_afastamento` (
@@ -1840,6 +1850,7 @@ CREATE TABLE IF NOT EXISTS `usuario_unid_tram` (
   KEY `idx_usuario` (`cod_usuario`),
   KEY `idx_unid_tramitacao` (`cod_unid_tramitacao`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 CREATE TABLE IF NOT EXISTS `vereadoresAtuais` (
 `cod_parlamentar` int(11)
 ,`nom_parlamentar` varchar(50)
@@ -1889,6 +1900,26 @@ CREATE TABLE IF NOT EXISTS `visita` (
   KEY `dat_entrada` (`dat_entrada`),
   KEY `des_situacao` (`des_situacao`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+DROP TABLE IF EXISTS `listaAutores`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `listaAutores`  AS  select distinct replace(`autor`.`cod_autor`,'L','') AS `cod_autor`,if((`tipo_autor`.`des_tipo_autor` = 'Parlamentar'),`parlamentar`.`nom_parlamentar`,if((`tipo_autor`.`des_tipo_autor` = 'Bancada'),concat(`bancada`.`nom_bancada`,' (',convert(date_format(`legislatura`.`dat_inicio`,'%Y') using utf8),'-',convert(date_format(`legislatura`.`dat_fim`,'%Y') using utf8),')'),if((`tipo_autor`.`des_tipo_autor` = 'Comissao'),`comissao`.`nom_comissao`,`autor`.`nom_autor`))) AS `nom_autor_join` from (`tipo_autor` join ((((`autor` left join `parlamentar` on(((`autor`.`cod_parlamentar` = `parlamentar`.`cod_parlamentar`) and (`parlamentar`.`ind_excluido` = 0)))) left join `comissao` on(((`autor`.`cod_comissao` = `comissao`.`cod_comissao`) and (`comissao`.`ind_excluido` = 0)))) left join `bancada` on(((`autor`.`cod_bancada` = `bancada`.`cod_bancada`) and (`bancada`.`ind_excluido` = 0)))) left join `legislatura` on((`bancada`.`num_legislatura` = `legislatura`.`num_legislatura`)))) where ((((`parlamentar`.`cod_parlamentar` is not null) and (`tipo_autor`.`des_tipo_autor` = 'Parlamentar')) or ((`bancada`.`cod_bancada` is not null) and (`tipo_autor`.`des_tipo_autor` = 'Bancada')) or ((`comissao`.`cod_comissao` is not null) and (`tipo_autor`.`des_tipo_autor` = 'Comissao')) or ((`tipo_autor`.`des_tipo_autor` <> 'Parlamentar') and (`tipo_autor`.`des_tipo_autor` <> 'Bancada') and (`tipo_autor`.`des_tipo_autor` <> 'Comissao'))) and (`autor`.`ind_excluido` = 0) and (`autor`.`tip_autor` = `tipo_autor`.`tip_autor`)) order by `nom_autor_join` ;
+
+DROP TABLE IF EXISTS `listaComissoes`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `listaComissoes`  AS  select `c`.`cod_comissao` AS `cod_comissao`,`c`.`sgl_comissao` AS `sgl_comissao`,`c`.`nom_comissao` AS `nom_comissao`,`c`.`dat_criacao` AS `dat_criacao`,`c`.`dat_extincao` AS `dat_extincao`,`tc`.`nom_tipo_comissao` AS `tipo_comissao` from (`comissao` `c` left join `tipo_comissao` `tc` on((`c`.`tip_comissao` = `tc`.`tip_comissao`))) where ((`c`.`ind_excluido` = 0) and (`tc`.`ind_excluido` = 0)) order by `c`.`nom_comissao` ;
+
+DROP TABLE IF EXISTS `listaMembrosComissoes`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `listaMembrosComissoes`  AS  select `cp`.`cod_comissao` AS `cod_comissao`,`c`.`nom_comissao` AS `nom_comissao`,`cp`.`cod_parlamentar` AS `cod_parlamentar`,`p`.`nom_completo` AS `nom_completo`,`p`.`nom_parlamentar` AS `nom_parlamentar`,`cc`.`des_cargo` AS `des_cargo`,`cp`.`ind_titular` AS `ind_titular`,date_format(`cp`.`dat_designacao`,'%d/%m/%Y') AS `dat_designacao`,date_format(`cp`.`dat_desligamento`,'%d/%m/%Y') AS `dat_desligamento`,`cp`.`des_motivo_desligamento` AS `des_motivo_desligamento`,`cp`.`obs_composicao` AS `obs_composicao` from ((((`periodo_comp_comissao` `pc` left join `composicao_comissao` `cp` on((`cp`.`cod_periodo_comp` = `pc`.`cod_periodo_comp`))) left join `cargo_comissao` `cc` on((`cp`.`cod_cargo` = `cc`.`cod_cargo`))) left join `parlamentar` `p` on((`cp`.`cod_parlamentar` = `p`.`cod_parlamentar`))) left join `comissao` `c` on((`cp`.`cod_comissao` = `c`.`cod_comissao`))) where ((cast(now() as date) >= `pc`.`dat_inicio_periodo`) and (cast(now() as date) <= `pc`.`dat_fim_periodo`) and (`pc`.`ind_excluido` = 0) and (`cp`.`ind_excluido` = 0)) order by `c`.`nom_comissao`,`cc`.`cod_cargo` ;
+
+DROP TABLE IF EXISTS `mesaAtual`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `mesaAtual`  AS  select distinct `cm`.`cod_parlamentar` AS `cod_parlamentar`,`cme`.`des_cargo` AS `des_cargo`,`p`.`nom_parlamentar` AS `nom_parlamentar`,`p`.`nom_completo` AS `nom_completo`,`pr`.`sgl_partido` AS `sgl_partido`,concat('https://publico.camararibeiraopreto.sp.gov.br/sapl_documentos/parlamentar/fotos/',`p`.`cod_parlamentar`,'_foto_parlamentar') AS `foto_parlamentar`,`p`.`end_email` AS `end_email` from (((((`composicao_mesa` `cm` left join `periodo_comp_mesa` `pm` on((`cm`.`cod_periodo_comp` = `pm`.`cod_periodo_comp`))) left join `cargo_mesa` `cme` on((`cm`.`cod_cargo` = `cme`.`cod_cargo`))) left join `parlamentar` `p` on((`cm`.`cod_parlamentar` = `p`.`cod_parlamentar`))) left join `filiacao` `f` on((`f`.`cod_parlamentar` = `p`.`cod_parlamentar`))) left join `partido` `pr` on((`f`.`cod_partido` = `pr`.`cod_partido`))) where ((cast(now() as date) >= `pm`.`dat_inicio_periodo`) and (cast(now() as date) <= `pm`.`dat_fim_periodo`) and (cast(now() as date) >= `f`.`dat_filiacao`) and isnull(`f`.`dat_desfiliacao`)) order by `cm`.`cod_cargo` ;
+
+DROP TABLE IF EXISTS `vereadoresAtuais`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vereadoresAtuais`  AS  select `p`.`cod_parlamentar` AS `cod_parlamentar`,`p`.`nom_parlamentar` AS `nom_parlamentar`,`p`.`nom_completo` AS `nom_completo`,`pr`.`sgl_partido` AS `sgl_partido`,`pr`.`cod_partido` AS `cod_partido`,concat('https://publico.camararibeiraopreto.sp.gov.br/sapl_documentos/parlamentar/fotos/',`p`.`cod_parlamentar`,'_foto_parlamentar') AS `foto_parlamentar`,`p`.`end_email` AS `end_email`,`p`.`txt_biografia` AS `txt_biografia`,`l`.`num_legislatura` AS `num_legislatura`,`m`.`dat_inicio_mandato` AS `dat_inicio_mandato`,`m`.`dat_fim_mandato` AS `dat_fim_mandato`,`m`.`ind_titular` AS `ind_titular`,`p`.`ind_ativo` AS `ind_ativo` from ((((`mandato` `m` left join `legislatura` `l` on((`m`.`num_legislatura` = `l`.`num_legislatura`))) left join `parlamentar` `p` on((`m`.`cod_parlamentar` = `p`.`cod_parlamentar`))) left join `filiacao` `f` on((`f`.`cod_parlamentar` = `p`.`cod_parlamentar`))) left join `partido` `pr` on((`f`.`cod_partido` = `pr`.`cod_partido`))) where ((cast(now() as date) >= `l`.`dat_inicio`) and (cast(now() as date) <= `l`.`dat_fim`) and (cast(now() as date) >= `f`.`dat_filiacao`) and isnull(`f`.`dat_desfiliacao`)) group by `p`.`cod_parlamentar` order by `p`.`nom_completo` ;
 
 ALTER TABLE `autor` ADD FULLTEXT KEY `nom_autor` (`nom_autor`);
 
