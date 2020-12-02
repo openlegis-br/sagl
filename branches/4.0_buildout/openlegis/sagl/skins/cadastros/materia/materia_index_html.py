2f5d3601c642f2716adbf19882ce1a7a83782fdc
OpenLegis <contato@openlegis.com.br>
2020-12-02 11:06:25 -0300

    Redirect cadastro

---
+++ branches/4.0_buildout/openlegis/sagl/skins/cadastros/materia/materia_index_html.py
@@ -0,0 +1,4 @@
+request=context.REQUEST
+redirect_url=context.portal_url()+'/cadastros/materia/materia_form_resumido'
+request.RESPONSE.redirect(redirect_url)
+~                                        
