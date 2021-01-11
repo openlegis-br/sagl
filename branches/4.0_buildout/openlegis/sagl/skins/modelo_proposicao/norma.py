2de53b3570069e5132367f8fb11d341195fedd34
OpenLegis <contato@openlegis.com.br>
2021-01-11 18:56:12 -0300

    Nome do prefeito em geração de matérias e normas

--- branches/4.0_buildout/openlegis/sagl/skins/modelo_proposicao/norma.py
+++ branches/4.0_buildout/openlegis/sagl/skins/modelo_proposicao/norma.py
@@ -32,6 +32,9 @@ for local in context.zsql.localidade_obter_zsql(cod_localidade = casa['cod_local
     inf_basicas_dic['nom_localidade']= local.nom_localidade
     inf_basicas_dic['sgl_uf']= local.sgl_uf
 
+inf_basicas_dic['nom_prefeito'] = 'Não Cadastrado'
+inf_basicas_dic['par_prefeito'] = 'Não Cadastrado'
+
 for norma in context.zsql.norma_juridica_obter_zsql(cod_norma=cod_norma):
  nom_arquivo = str(norma.cod_norma)+'_texto_integral.odt'
  des_tipo_norma = norma.des_tipo_norma.decode('utf-8').upper()
@@ -41,5 +44,8 @@ for norma in context.zsql.norma_juridica_obter_zsql(cod_norma=cod_norma):
  data_norma = context.pysc.data_converter_por_extenso_pysc(data=norma.dat_norma).decode('utf-8').upper()
  data_norma2 = context.pysc.data_converter_por_extenso_pysc(data=norma.dat_norma).decode('utf-8')
  txt_ementa = norma.txt_ementa
+ for prefeito in context.zsql.prefeito_atual_obter_zsql(data_composicao = norma.dat_norma):
+     inf_basicas_dic['nom_prefeito'] = prefeito.nom_completo
+     inf_basicas_dic['par_prefeito'] = prefeito.sgl_partido   
 
 return st.norma_gerar_odt(inf_basicas_dic, nom_arquivo, des_tipo_norma, num_norma, ano_norma, dat_norma, data_norma, txt_ementa, modelo_norma)
