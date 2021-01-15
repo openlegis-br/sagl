72ecedd3339fa87a22fe0f5c0b712821f5d87e76
OpenLegis <contato@openlegis.com.br>
2021-01-15 13:32:12 -0300

    geração parecer comissão

--- branches/4.0_buildout/openlegis/sagl/skins/modelo_proposicao/parecer.py
+++ branches/4.0_buildout/openlegis/sagl/skins/modelo_proposicao/parecer.py
@@ -33,6 +33,7 @@ for local in context.zsql.localidade_obter_zsql(cod_localidade = casa['cod_local
     inf_basicas_dic['sgl_uf']= local.sgl_uf
 
 for relatoria in context.zsql.relatoria_obter_zsql(cod_relatoria=cod_relatoria, cod_materia=cod_materia, cod_comissao=cod_comissao, ind_excluido=0):
+ nom_arquivo = str(relatoria.cod_relatoria)+ '_parecer.odt'
  tip_apresentacao = ""
  if relatoria.tip_apresentacao == 'O':
   tip_apresentacao = 'Verbal'
@@ -61,14 +62,16 @@ for relatoria in context.zsql.relatoria_obter_zsql(cod_relatoria=cod_relatoria, 
  for periodo in context.zsql.periodo_comp_comissao_obter_zsql(data=DateTime(relatoria.dat_desig_relator),cod_comissao=relatoria.cod_comissao):
   cod_periodo = periodo.cod_periodo_comp
 
+ nom_presidente_comissao = ""
  lst_composicao = []
  for composicao_comissao in context.zsql.composicao_comissao_obter_zsql(cod_comissao=relatoria.cod_comissao,cod_periodo_comp=periodo.cod_periodo_comp):
+  if composicao_comissao.des_cargo == 'Presidente':
+     nom_presidente_comissao = composicao_comissao.nom_completo
+     inf_basicas_dic['nom_presidente_comissao'] = nom_presidente_comissao
   dic_composicao = {}
-  if composicao_comissao.nom_completo != nom_relator:
-   dic_composicao["nom_completo"] = composicao_comissao.nom_completo
-   lst_composicao.append(dic_composicao)
-
- nom_arquivo = str(relatoria.cod_relatoria)+ '_parecer.odt'
+  if composicao_comissao.nom_completo != nom_relator and composicao_comissao.nom_completo != nom_presidente_comissao:
+     dic_composicao["nom_completo"] = composicao_comissao.nom_completo
+     lst_composicao.append(dic_composicao)
 
  for materia_vinculada in context.zsql.materia_obter_zsql(cod_materia = relatoria.cod_materia):
   materia = materia_vinculada.des_tipo_materia.decode('utf-8').upper() + ' Nº ' + str(materia_vinculada.num_ident_basica) + '/' + str(materia_vinculada.ano_ident_basica)
