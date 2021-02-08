6bb0c54ad477b3be16b69aaa52645a5981dc2d41
OpenLegis <contato@openlegis.com.br>
2021-02-08 16:22:09 -0300

    Recebimento proposição - matéria principal

--- branches/4.0_buildout/openlegis/sagl/skins/cadastros/recebimento_proposicao/criar_materia_pysc.py
+++ branches/4.0_buildout/openlegis/sagl/skins/cadastros/recebimento_proposicao/criar_materia_pysc.py
@@ -60,7 +60,7 @@ def criar_materia(tip_materia, num_ident_basica, ano_materia, dat_apresentacao, 
 def inserir_autoria(cod_materia, cod_autor, cod_proposicao):
     context.zsql.autoria_incluir_zsql(cod_autor = cod_autor, cod_materia = cod_materia, ind_primeiro_autor = 1)
     
-    return autuar_materia(cod_materia)
+    return autuar_materia(cod_materia, cod_proposicao)
 
 
 def autuar_materia(cod_materia, cod_proposicao):
