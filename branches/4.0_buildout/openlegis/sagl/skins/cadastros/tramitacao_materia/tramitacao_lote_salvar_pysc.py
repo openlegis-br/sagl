274acc56321466411ce1f97578c2a3c17ab2870e
OpenLegis <contato@openlegis.com.br>
2021-03-11 12:07:10 -0300

    Calculo automático de prazo nas tramitações em lote

--- branches/4.0_buildout/openlegis/sagl/skins/cadastros/tramitacao_materia/tramitacao_lote_salvar_pysc.py
+++ branches/4.0_buildout/openlegis/sagl/skins/cadastros/tramitacao_materia/tramitacao_lote_salvar_pysc.py
@@ -33,8 +33,19 @@ if lst_ultimas != []:
        context.zsql.tramitacao_ind_ultima_atualizar_zsql(cod_materia = dic['cod_materia'], cod_tramitacao = dic['cod_tramitacao'], ind_ult_tramitacao = 0)
        context.zsql.tramitacao_registrar_recebimento_zsql(cod_tramitacao = dic['cod_tramitacao'], cod_usuario_corrente = hdn_cod_usuario_local)    
 
+if txt_dat_fim_prazo==None or txt_dat_fim_prazo=='':
+   data_atual = DateTime()
+   for tramitacao in context.zsql.status_tramitacao_obter_zsql(cod_status=lst_cod_status, ind_excluido=0):
+       if tramitacao.num_dias_prazo != None:
+          data_calculada = data_atual + str(tramitacao.num_dias_prazo)
+          txt_dat_fim_prazo = DateTime(data_calculada).strftime('%Y/%m/%d')
+       else:
+          txt_dat_fim_prazo = ''
+elif txt_dat_fim_prazo != '':
+   txt_dat_fim_prazo = DateTime(txt_dat_fim_prazo).strftime('%Y/%m/%d')
+
 for item in cod_materia:
-    context.zsql.tramitacao_incluir_zsql(cod_materia = item, dat_tramitacao = context.pysc.data_converter_pysc(data=txt_dat_tramitacao), cod_unid_tram_local = unidade_local, cod_usuario_local = hdn_cod_usuario_local, cod_unid_tram_dest = lst_cod_unid_tram_dest, cod_usuario_dest = lst_cod_usuario_dest, dat_encaminha = hdn_dat_encaminha, cod_status = lst_cod_status, ind_urgencia = rad_ind_urgencia, txt_tramitacao = txa_txt_tramitacao, dat_fim_prazo = context.pysc.data_converter_pysc(data=txt_dat_fim_prazo), ind_ult_tramitacao = 1)
+    context.zsql.tramitacao_incluir_zsql(cod_materia = item, dat_tramitacao = context.pysc.data_converter_pysc(data=txt_dat_tramitacao), cod_unid_tram_local = unidade_local, cod_usuario_local = hdn_cod_usuario_local, cod_unid_tram_dest = lst_cod_unid_tram_dest, cod_usuario_dest = lst_cod_usuario_dest, dat_encaminha = hdn_dat_encaminha, cod_status = lst_cod_status, ind_urgencia = rad_ind_urgencia, txt_tramitacao = txa_txt_tramitacao, dat_fim_prazo = txt_dat_fim_prazo, ind_ult_tramitacao = 1)
 
 lst_novas = []
 for item in cod_materia:
