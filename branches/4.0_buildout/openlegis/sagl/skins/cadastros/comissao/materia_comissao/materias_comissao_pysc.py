5543c8aa0a9892b76dadca0de93e34c3946ecb15
OpenLegis <contato@openlegis.com.br>
2021-05-26 23:00:06 -0300

    Matérias comissão - apenas am tramitação

--- branches/4.0_buildout/openlegis/sagl/skins/cadastros/comissao/materia_comissao/materias_comissao_pysc.py
+++ branches/4.0_buildout/openlegis/sagl/skins/cadastros/comissao/materia_comissao/materias_comissao_pysc.py
@@ -10,7 +10,9 @@
 materias = []
 for despacho in context.zsql.despacho_inicial_obter_zsql(cod_comissao=cod_comissao, ind_excluido=0):
     if not context.zsql.relatoria_obter_zsql(cod_comissao=cod_comissao,cod_materia=despacho.cod_materia,num_ordem=despacho.num_ordem):
-       materias.append(int(despacho.cod_materia))
+       for materia in context.zsql.materia_obter_zsql(cod_materia=despacho.cod_materia, ind_excluido=0):
+           if materia.ind_tramitacao == 1:
+              materias.append(int(despacho.cod_materia))
 
 return materias
 
