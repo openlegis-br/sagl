c89bc901e7de743fce0983fc7a61278071960c35
OpenLegis <contato@openlegis.com.br>
2021-07-12 21:26:48 -0300

    Corrige email

--- branches/4.0_buildout/openlegis/sagl/skins/pysc/password_email.py
+++ branches/4.0_buildout/openlegis/sagl/skins/pysc/password_email.py
@@ -13,10 +13,22 @@ from Products.CMFDefault.utils import decode
 from Products.CMFDefault.utils import makeEmail
 from Products.CMFDefault.utils import Message as _
 
+request=context.REQUEST
+response=request.RESPONSE
+session= request.SESSION
+
+
 atool = getToolByName(script, 'portal_actions')
 ptool = getUtilityByInterfaceName('Products.CMFCore.interfaces.IPropertiesTool')
 utool = getToolByName(script, 'portal_url')
 portal_url = utool()
+
+casa={}
+aux=context.sapl_documentos.props_sagl.propertyItems()
+for item in aux:
+  casa[item[0]] = item[1]
+email_casa = casa['end_email_casa']
+casa_legislativa = casa['nom_casa']
 
 options = {}
 options['password'] = password
@@ -24,8 +36,8 @@ options['password'] = password
 headers = {}
 headers['Subject'] = _(u'${portal_title}: Lembrete de senha',
                       mapping={'portal_title': decode(ptool.title(), script)})
-headers['From'] = '%s <%s>' % (ptool.getProperty('email_from_name'),
-                               ptool.getProperty('email_from_address'))
+headers['From'] = '%s <%s>' % (casa_legislativa,
+                               email_casa)
 headers['To'] = '<%s>' % (email)
 
 mtext = context.generico.password_email_template(**decode(options, script))
