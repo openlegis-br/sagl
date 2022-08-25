202b7d0fab5cc09f04b2f30dec9f49b5a14517d1
OpenLegis <contato@openlegis.com.br>
2022-08-25 10:50:29 -0300

    Processo adm integral em método externo

---
+++ branches/4.0_buildout/openlegis/sagl/skins/pysc/proc_adm_integral.py
@@ -0,0 +1,11 @@
+## Script (Python) "proc_adm_integral"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=cod_documento
+##title=
+##
+return context.extensions.processo_adm(context)
+
