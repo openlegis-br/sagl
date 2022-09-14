02c07f94e99107528e29a6d68400e359acdf3a58
OpenLegis <contato@openlegis.com.br>
2022-09-14 16:54:32 -0300

    Permissão peticionamento

--- branches/4.0_buildout/openlegis/sagl/skins/cadastros/auxiliares/tipo_peticionamento/usuarios_permitir_pysc.py
+++ branches/4.0_buildout/openlegis/sagl/skins/cadastros/auxiliares/tipo_peticionamento/usuarios_permitir_pysc.py
@@ -4,9 +4,15 @@
 ##bind namespace=
 ##bind script=script
 ##bind subpath=traverse_subpath
-##parameters=tip_peticionamento,cod_usuario=""
+##parameters=tip_peticionamento, cod_usuario=""
 ##title=
 ##
+
+v=str(cod_usuario)
+if v.isdigit():
+   lst_usuario = [cod_usuario]
+else:
+   lst_usuario = cod_usuario
 
 lista_usuarios=context.zsql.usuario_peticionamento_obter_zsql(tip_peticionamento=tip_peticionamento)
 
@@ -15,15 +21,16 @@ usuarios=[]
 for usuario in lista_usuarios:
   usuarios.append(str(usuario.cod_usuario))
 
-if cod_usuario == ['0']:
-  context.zsql.usuario_peticionamento_excluir_zsql(tip_peticionamento=tip_peticionamento, cod_usuario=cod_usuario)
+if lst_usuario == '' and tip_peticionamento != '':
+  for item in usuarios:
+      context.zsql.usuario_peticionamento_excluir_zsql(tip_peticionamento=tip_peticionamento, cod_usuario=item)
 
-elif cod_usuario != ['0']:
-  for i in cod_usuario:
+elif lst_usuario != '':
+  for i in lst_usuario:
     if str(i) not in usuarios:
       context.zsql.usuario_peticionamento_incluir_zsql(tip_peticionamento=tip_peticionamento, cod_usuario=i)
   for i in usuarios:
-    if str(i) not in cod_usuario:
+    if str(i) not in lst_usuario:
       context.zsql.usuario_peticionamento_excluir_zsql(tip_peticionamento=tip_peticionamento, cod_usuario=i)
 
 return 1
