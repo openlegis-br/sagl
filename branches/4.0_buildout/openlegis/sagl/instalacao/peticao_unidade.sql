06e69f99a405ec3df051d486fca83a8ec4902efa
OpenLegis <contato@openlegis.com.br>
2022-10-15 16:58:14 -0300

    Correção SQL1

--- branches/4.0_buildout/openlegis/sagl/instalacao/peticao_unidade.sql
+++ branches/4.0_buildout/openlegis/sagl/instalacao/peticao_unidade.sql
@@ -1,5 +1,5 @@
 ALTER TABLE tipo_peticionamento CHANGE `cod_unid_tram_dest` `cod_unid_tram_dest` INT NULL;
 
-ALTER TABLE tipo_peticionamento DROP FOREIGN KEY tipo_peticionamento_ibfk_1`;`
+ALTER TABLE tipo_peticionamento DROP FOREIGN KEY tipo_peticionamento_ibfk_1;
 
 ALTER TABLE peticao ADD `cod_unid_tram_dest` INT NULL AFTER `txt_observacao`;
