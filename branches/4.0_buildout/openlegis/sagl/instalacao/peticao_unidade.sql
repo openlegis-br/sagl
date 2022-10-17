98d04188b0222f90be2ae8998f9ef4a5f6664d0b
OpenLegis <contato@openlegis.com.br>
2022-10-17 14:29:00 -0300

    Permissões por usuário por tipo no módulo de documentos x processos administrativos

--- branches/4.0_buildout/openlegis/sagl/instalacao/peticao_unidade.sql
+++ branches/4.0_buildout/openlegis/sagl/instalacao/peticao_unidade.sql
@@ -13,7 +13,7 @@ ALTER TABLE `usuario_tipo_documento`
 
 ALTER TABLE tipo_peticionamento CHANGE `cod_unid_tram_dest` `cod_unid_tram_dest` INT NULL;
 
-ALTER TABLE tipo_peticionamento DROP FOREIGN KEY tipo_peticionamento_ibfk_1`;
+ALTER TABLE tipo_peticionamento DROP FOREIGN KEY tipo_peticionamento_ibfk_1;
 
 ALTER TABLE peticao ADD `cod_unid_tram_dest` INT NULL AFTER `txt_observacao`;
 
