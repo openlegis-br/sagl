5caa340782cc1b6ee8c8b9c9d3be51974fe1d08a
OpenLegis <contato@openlegis.com.br>
2022-10-18 17:27:49 -0300

    Melhoria permissão

--- branches/4.0_buildout/openlegis/sagl/skins/consultas/documento_administrativo/permissao_tipo.py
+++ branches/4.0_buildout/openlegis/sagl/skins/consultas/documento_administrativo/permissao_tipo.py
@@ -11,12 +11,12 @@ request=context.REQUEST
 
 response=request.RESPONSE
 
-if request['AUTHENTICATED_USER'].has_role(['Authenticated']):
-   for usuario in context.zsql.usuario_obter_zsql(col_username=request['AUTHENTICATED_USER'].getUserName()):
-       if usuario.cod_usuario:
-          cod_usuario_corrente = int(usuario.cod_usuario)
-       else:
-          cod_usuario_corrente = 0
+for usuario in context.zsql.usuario_obter_zsql(col_username=request['AUTHENTICATED_USER'].getUserName()):
+    if usuario.cod_usuario:
+       cod_usuario_corrente = int(usuario.cod_usuario)
+    else:
+       cod_usuario_corrente = 0
+
 
 lst_tipo=[]
 
@@ -24,35 +24,26 @@ for tipo in context.zsql.tipo_documento_administrativo_obter_zsql(ind_escluido=0
     if request['AUTHENTICATED_USER'].has_role(['Manager', 'Operador', 'Operador Modulo Administrativo', 'Consulta Modulo Administrativo']):
        dic_tipo= {}
        dic_tipo['tip_documento']= tipo.tip_documento
-       dic_tipo['sgl_tipo_documento']= tipo.sgl_tipo_documento
        dic_tipo['des_tipo_documento']= tipo.des_tipo_documento
        dic_tipo['ind_publico']= tipo.ind_publico 
-       dic_tipo['ind_incluir']= 1 
        lst_tipo.append(dic_tipo)
-    elif request['AUTHENTICATED_USER'].has_role([ 'Operador Materia']):
+    elif request['AUTHENTICATED_USER'].has_role(['Operador Materia']):
        if str(tipo.ind_publico) == '1':
           dic_tipo= {}
           dic_tipo['tip_documento']= tipo.tip_documento
-          dic_tipo['sgl_tipo_documento']= tipo.sgl_tipo_documento
           dic_tipo['des_tipo_documento']= tipo.des_tipo_documento 
           dic_tipo['ind_publico']= tipo.ind_publico 
-          dic_tipo['ind_incluir']= 1 
           lst_tipo.append(dic_tipo) 
     elif request['AUTHENTICATED_USER'].has_role(['Authenticated']):
-       if str(tipo.ind_publico) == '1' or context.zsql.usuario_tipo_documento_obter_zsql(tip_documento=tipo.tip_documento, cod_usuario=cod_usuario_corrente, ind_excluido=0):
+       if str(tipo.ind_publico) == '1' and context.zsql.usuario_tipo_documento_obter_zsql(cod_usuario=cod_usuario_corrente, ind_excluido=0):
           dic_tipo= {}
           dic_tipo['tip_documento']= tipo.tip_documento
-          dic_tipo['sgl_tipo_documento']= tipo.sgl_tipo_documento
           dic_tipo['des_tipo_documento']= tipo.des_tipo_documento 
-          dic_tipo['ind_publico']= tipo.ind_publico
-          dic_tipo['ind_incluir']= 0 
-          if context.zsql.usuario_tipo_documento_obter_zsql(tip_documento=tipo.tip_documento, cod_usuario=cod_usuario_corrente, ind_excluido=0):
-             dic_tipo['ind_incluir']= 1 
+          dic_tipo['ind_publico']= tipo.ind_publico 
           lst_tipo.append(dic_tipo) 
     elif tipo.ind_publico == 1:
        dic_tipo= {}
        dic_tipo['tip_documento']= tipo.tip_documento
-       dic_tipo['sgl_tipo_documento']= tipo.sgl_tipo_documento
        dic_tipo['des_tipo_documento']= tipo.des_tipo_documento 
        dic_tipo['ind_publico']= tipo.ind_publico 
        lst_tipo.append(dic_tipo)  
