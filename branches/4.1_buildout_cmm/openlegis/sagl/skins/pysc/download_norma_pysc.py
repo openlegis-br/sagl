55eb8ac9b2bf20f02fa1384173ebf78b5f68ab83
OpenLegis <contato@openlegis.com.br>
2023-02-10 12:59:33 -0300

    Update download_norma_pysc.py

--- branches/4.1_buildout_cmm/openlegis/sagl/skins/pysc/download_norma_pysc.py
+++ branches/4.1_buildout_cmm/openlegis/sagl/skins/pysc/download_norma_pysc.py
@@ -13,13 +13,13 @@ response=request.RESPONSE
 for norma in context.zsql.norma_juridica_obter_zsql(cod_norma=cod_norma):
   if texto_original:
     if hasattr(context.sapl_documentos.norma_juridica, "%s"%cod_norma+'_texto_integral_signed.pdf'):
-       download_name = norma.des_tipo_norma.replace(" ", "_")+"-"+str(norma.num_norma)+"-"+str(norma.ano_norma)+"-signed.pdf"
+       download_name = str(norma.des_tipo_norma).replace(" ", "_")+"-"+str(norma.num_norma)+"-"+str(norma.ano_norma)+"-signed.pdf"
        id_documento = "%s"%cod_norma+'_texto_integral_signed.pdf'
     elif hasattr(context.sapl_documentos.norma_juridica, "%s"%cod_norma+'_texto_integral.pdf'):
-       download_name = norma.des_tipo_norma.replace(" ", "_")+"-"+str(norma.num_norma)+"-"+str(norma.ano_norma)+"-original.pdf"
+       download_name = str(norma.des_tipo_norma).replace(" ", "_")+"-"+str(norma.num_norma)+"-"+str(norma.ano_norma)+"-original.pdf"
        id_documento = "%s"%cod_norma+'_texto_integral.pdf'
   if texto_consolidado:
-    download_name = norma.des_tipo_norma.replace(" ", "_")+"-"+str(norma.num_norma)+"-"+str(norma.ano_norma)+"_consolidacao.pdf"
+    download_name = str(norma.des_tipo_norma).replace(" ", "_")+"-"+str(norma.num_norma)+"-"+str(norma.ano_norma)+"_consolidacao.pdf"
     id_documento = "%s"%cod_norma+'_texto_consolidado.pdf'
   arquivo = getattr(context.sapl_documentos.norma_juridica,id_documento) 
 
