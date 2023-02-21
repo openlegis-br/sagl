dc4e28f13c9737c643b132bbc9bc688e6ccbd159
OpenLegis <contato@openlegis.com.br>
2023-02-21 11:36:16 -0300

    Update ajusta_permissao.py

--- branches/4.1_buildout/openlegis/sagl/skins/cadastros/auxiliares/tipo_documento_administrativo/ajusta_permissao.py
+++ branches/4.1_buildout/openlegis/sagl/skins/cadastros/auxiliares/tipo_documento_administrativo/ajusta_permissao.py
@@ -13,28 +13,28 @@ for documento in context.zsql.documento_administrativo_pesquisar_publico_zsql(ti
  assinado = str(documento.cod_documento) + "_texto_integral_signed.pdf"
  if hasattr(context.sapl_documentos.administrativo, filename):
    if context.REQUEST.ind_publico == 0:
-     documento = getattr(context.sapl_documentos.administrativo,filename)
-     documento.manage_permission('View', roles=['Authenticated', 'Manager','Operador','Operador Modulo Administrativo'], acquire=0)
+     file = getattr(context.sapl_documentos.administrativo,filename)
+     file.manage_permission('View', roles=['Authenticated'], acquire=0)
    if context.REQUEST.ind_publico == 1:
-     documento = getattr(context.sapl_documentos.administrativo,filename)
-     documento.manage_permission('View', roles=['Anonymous','Manager','Operador','Operador Materia','Operador Modulo Administrativo'], acquire=1)
-     documento.manage_permission('Delete objects', roles=['Manager','Operador','Operador Materia','Operador Modulo Administrativo'], acquire=1)
+     file = getattr(context.sapl_documentos.administrativo,filename)
+     file.manage_permission('View', roles=['Anonymous', 'Authenticated'], acquire=0)
+     file.manage_permission('Delete objects', roles=['Manager','Operador','Operador Materia','Operador Modulo Administrativo'], acquire=0)
  if hasattr(context.sapl_documentos.administrativo, assinado):
    if context.REQUEST.ind_publico == 0:
-     documento = getattr(context.sapl_documentos.administrativo,assinado)
-     documento.manage_permission('View', roles=['Authenticated', 'Manager','Operador','Operador Modulo Administrativo'], acquire=0)
+     file = getattr(context.sapl_documentos.administrativo,assinado)
+     file.manage_permission('View', roles=['Authenticated'], acquire=0)
    if context.REQUEST.ind_publico == 1:
-     documento = getattr(context.sapl_documentos.administrativo,assinado)
-     documento.manage_permission('View', roles=['Anonymous','Manager','Operador','Operador Materia','Operador Modulo Administrativo'], acquire=1)
-     documento.manage_permission('Delete objects', roles=['Manager','Operador','Operador Materia','Operador Modulo Administrativo'], acquire=1)
+     file = getattr(context.sapl_documentos.administrativo,assinado)
+     file.manage_permission('View', roles=['Anonymous', 'Authenticated'], acquire=0)
+     file.manage_permission('Delete objects', roles=['Manager','Operador','Operador Materia','Operador Modulo Administrativo'], acquire=0)
 
-for documento in context.zsql.documento_acessorio_administrativo_obter_zsql(tip_documento=tip_documento,ind_excluido=0):
- filename_acessorio = str(documento.cod_documento_acessorio) + ".pdf"
- if hasattr(context.sapl_documentos.administrativo, filename_acessorio):
-   if context.REQUEST.ind_publico == 0:
-     documento = getattr(context.sapl_documentos.administrativo,filename_acessorio)
-     documento.manage_permission('View', roles=['Authenticated','Manager','Operador','Operador Modulo Administrativo'], acquire=0)
-   if context.REQUEST.ind_publico == 1:
-     documento = getattr(context.sapl_documentos.administrativo,filename_acessorio)
-     documento.manage_permission('View', roles=['Anonymous','Manager','Operador','Operador Materia','Operador Modulo Administrativo'], acquire=1)
-     documento.manage_permission('Delete objects', roles=['Manager','Operador','Operador Materia','Operador Modulo Administrativo'], acquire=1)
+ for doc in context.zsql.documento_acessorio_administrativo_obter_zsql(cod_documento=documento.cod_documento,ind_excluido=0):
+  filename_acessorio = str(doc.cod_documento_acessorio) + ".pdf"
+  if hasattr(context.sapl_documentos.administrativo, filename_acessorio):
+    if context.REQUEST.ind_publico == 0:
+      file = getattr(context.sapl_documentos.administrativo,filename_acessorio)
+      file.manage_permission('View', roles=['Authenticated'], acquire=0)
+    if context.REQUEST.ind_publico == 1:
+      file = getattr(context.sapl_documentos.administrativo,filename_acessorio)
+      file.manage_permission('View', roles=['Anonymous', 'Authenticated'], acquire=0)
+      file.manage_permission('Delete objects', roles=['Manager','Operador','Operador Materia','Operador Modulo Administrativo'], acquire=0)
